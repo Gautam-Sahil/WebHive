5b59265a7236944e077f8ff81f5701d6248236fc
Gautam-Sahil <193165968+Gautam-Sahil@users.noreply.github.com>
2025-12-08 04:07:02 +0530

    sandbox is working

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,18 @@
 
-
+import { Sandbox } from "@e2b/code-interpreter";
 import { createAgent, openai } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
-
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () =>{
+      const sandbox = await Sandbox.create("webhive-nextjs-test")
+      return sandbox.sandboxId;
+    });
     const summarizer = createAgent({
       name: "summarizer",
       system: "You are next.js developer. you write the readable and maintnable code. you write simple next.js and react snippets",
@@ -28,6 +33,13 @@ export const helloWorld = inngest.createFunction(
     );
 
     console.log("Agent Output:", output);
-    return { output };
+
+ const sandboxUrl = await step.run("get-sandbox-url", async () =>{
+   const sandbox = await getSandbox(sandboxId);
+   const host = sandbox.getHost(3000);
+   return `https://${host}`;
+ })
+
+    return { output, sandboxUrl };
   },
 );
